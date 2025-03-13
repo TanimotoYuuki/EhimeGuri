a4d42bbbcc2572f6db8de073d222f7c1ed44a4a6
YamaguchiHayato <CA01244029@st.kawahara.ac.jp>
2025-03-13 13:21:48 +0900

    tkmファイルのファイルパスの移動とMovingFloorクラスの追加

--- GameTemplate/Game/Game.cpp
+++ GameTemplate/Game/Game.cpp
@@ -1,18 +1,34 @@
 #include "stdafx.h"
 #include "BackGround.h"
-#include "Game.h"
 #include "Player.h"
-#include"GameCamera.h"
+//#include "MovingFloor.h"
+#include "Game.h"
+#include "GameCamera.h"
+
+//namespace
+//{
+//	Vector3 BACKGROUND_SCALE = Vector3(0.7f, 1.0f, 0.7f);
+//}
 
 bool Game::Start()
 {
+
 	NewGO<Player>(0, "player");
 	NewGO<BackGround>(0, "background");
 	NewGO<GameCamera>(0, "gamecamera");
+
+//↓コメントアウトを解除する場合Playerを1にする
+// 	その際、カメラを2, UIのCPPは2
+//	NewGO<MovingFloor>(0, "movingfloor");
 	return true;
 }
 
 void Game::Update()
 {
 
+}
+
+void Game::Render(RenderContext& rc)
+{
+//	backGroundRender.Draw(rc);
 }
