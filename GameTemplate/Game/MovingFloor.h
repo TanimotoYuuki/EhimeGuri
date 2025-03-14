a0ced4cda397dcb686837d747f0adb103b58f110
YamaguchiHayato <CA01244029@st.kawahara.ac.jp>
2025-03-14 10:00:22 +0900

    MovigFloorクラスのアクティブ化 コメントと関数等の配置整理

--- GameTemplate/Game/MovingFloor.h
+++ GameTemplate/Game/MovingFloor.h
@@ -1,48 +1,52 @@
-//#pragma once 
-//
-//class Player;
-//class BackGround;
-///// <summary>
-///// 動く床。
-///// </summary>
-//class MovingFloor : public IGameObject
-//{
-//public:
-//	MovingFloor();
-//	~MovingFloor();
-//
-//	bool Start();
-//	void Update();
-//	void Render(RenderContext& rc);
-//	/// <summary>
-//	/// 座標を設定。
-//	/// </summary>
-//	void SetPosition(const Vector3& position)
-//	{
-//		m_position = position;
-//	}
-//
-//	const Vector3& GetPosition()const
-//	{
-//		return m_position;
-//	}
-//
-//private:
-//	/// <summary>
-//	/// 移動処理。
-//	/// </summary>
-//	void Move();
-//
-//	Vector3										m_position;
-//	Vector3										m_firstPosition;
-//	ModelRender									m_modelRender;
-//	Player									  * m_player = nullptr;
-//	PhysicsStaticObject							m_physicsStaticObject;				//静的物理オブジェクト。
-////	CollisionObject						      * m_collisionObject = nullptr;		//コリジョンオブジェクト。
-//	enum enMovingFloorState
-//	{
-//		enMovingFloorState_MovingRight,
-//		enMovingFloorState_MovingLeft
-//	};
-//	enMovingFloorState							m_movingFloorState = enMovingFloorState_MovingRight;
-//};
+#pragma once 
+
+class Player;
+class BackGround;
+/// <summary>
+/// 動く床。
+/// </summary>
+class MovingFloor : public IGameObject
+{
+public:
+	MovingFloor();
+	~MovingFloor();
+
+	bool Start();
+	void Update();
+	void Render(RenderContext& rc);
+	/// <summary>
+	/// 座標を設定。
+	/// </summary>
+	void SetPosition(const Vector3& position)
+	{
+		m_position = position;
+	}
+
+	const Vector3& GetPosition()const
+	{
+		return m_position;
+	}
+
+
+	Vector3										m_position;
+
+private:
+	/// <summary>
+	/// 移動処理。
+	/// </summary>
+	void Move();
+
+	Player									  * m_player = nullptr;
+	CollisionObject							  * m_collisionObject = nullptr;		//コリジョンオブジェクト。
+
+	Vector3										m_firstPosition;
+	ModelRender									m_modelRender;
+	PhysicsStaticObject							m_physicsStaticObject;				
+	
+	enum enMovingFloorState
+	{
+		enMovingFloorState_MovingRight,
+		enMovingFloorState_MovingLeft
+	};
+	enMovingFloorState							m_movingFloorState = enMovingFloorState_MovingRight;
+};
