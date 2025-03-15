df39c61299112f7193cd81539c4a66b7b84c3d3a
kai <CA01244006@st.kawahara.ac.jp>
2025-03-15 13:40:23 +0900

    スタミナゲージの追加

--- GameTemplate/Game/Game.h
+++ GameTemplate/Game/Game.h
@@ -21,5 +21,16 @@ private:
 	ModelRender   m_backGroundRender;
 	ModelRender	  m_modelRender;
 	Vector3		  m_position;
+	FontRender m_fontRender;
+
+	enum Sutaminastate {
+		SutaminaMax,
+		Sutamina0
+	};
+
+	Sutaminastate m_PlayerSutaminaSutate = SutaminaMax;
+	SpriteRender SutaminaMaxrender;
+	SpriteRender Sutamina0render;
+	Vector3 m_scale = Vector3::One;
 };
 
