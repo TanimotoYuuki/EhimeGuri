4777186e69b83328fa1c5628bfc25fac37a1fd61
谷本優樹 <yuki016.kawa@gmail.com>
2025-04-11 15:09:14 +0900

    ステージクリア演出の実装

---
+++ GameTemplate/Game/StageClear.h
@@ -0,0 +1,28 @@
+#pragma once
+class Player;
+class StageClear : public IGameObject
+{
+public:
+	bool Start();
+	void Update();
+
+	void Render(RenderContext& rc);
+private:
+	/// <summary>
+	/// ステージクリアスプライト用のイージングを設定
+	/// </summary>
+	void SetStageClearSpriteEasing();
+	/// <summary>
+	/// ステージクリアスプライト用のイージングの更新処理
+	/// </summary>
+	void UpdateStageClearSpriteEaging();
+
+	SpriteRender		m_stageClear;												//ステージクリア
+	Vector3				m_position = Vector3(-1100.0f, 275.0f, 0.0f);				//位置
+	Vector3				m_beforeEasingPosition;										//イージング前の位置
+	Vector3				m_afterEasingPosition;										//イージング後の位置
+	float               m_easingTime = 0.0f;										//割合
+	bool                m_gameClearRenditionFinishFlag = false;						//ゲームクリア演出終了フラグ
+	Player*				m_player;													//プレイヤー用のインスタンス
+};
+
