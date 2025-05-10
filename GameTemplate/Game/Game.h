30f4a1d2d24db3339c07fff31c84e220c2175cd8
YamaguchiHayato <CA01244029@st.kawahara.ac.jp>
2025-05-10 21:38:19 +0900

    コンフリクト修正

--- GameTemplate/Game/Game.h
+++ GameTemplate/Game/Game.h
@@ -257,18 +257,33 @@ private:
 	/// <summary>
 	/// タオル。
 	/// </summary>
-	Towel             * m_towel;
+	Towel* m_towel;
 	/// <summary>
 	/// フェード。
 	/// </summary>
-	Fade			  * m_fade;
-
+	Fade* m_fade;
+	/// <summary>
+	/// アイテム類。
+	/// </summary>
+	ItemEnemy* m_itemenemy; // 敵のアイテムドロップ。
+	Tobeyaki* m_tobeyaki;// 砥部焼き。
+	sinju* m_sinju;// 真珠。
+	Mikan* m_mikan;// みかん。
+	Jako* m_jako;// じゃこ天。
+	Taruto* Taruto;// タルト。
+	/// <summary>
+	/// フォントレンダー。
+	/// </summary>
 	FontRender          m_timerRender;
 	FontRender          m_fontRender;
-
+	/// <summary>
+	/// モデルレンダー。
+	/// </summary>
 	ModelRender         m_backGroundRender;
 	ModelRender	        m_modelRender;
-
+	/// <summary>
+	/// 静的物理オブジェクト。
+	/// </summary>
 	PhysicsStaticObject m_physicsStaticObject;
 	
 
