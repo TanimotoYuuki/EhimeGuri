c36ff0e107e0a9c8c113575618f450fde54630e8
谷本優樹 <yuki016.kawa@gmail.com>
2025-05-19 11:19:40 +0900

    Level3DRenderの修正

--- GameTemplate/k2EngineLow/Level3DRender.cpp
+++ GameTemplate/k2EngineLow/Level3DRender.cpp
@@ -1,5 +1,6 @@
 #include "k2EngineLowPreCompile.h"
 #include "Level3DRender.h"
+#include "level3D/MapChip.h"
 #include "level3D/TklFile.h"
 
 namespace Level3DConst
@@ -20,6 +21,14 @@ namespace nsK2EngineLow
 	{
 	}
 
+	//マップチップを作成
+	void Level3DRender::CreateMapChip(const LevelObjectData& objData, const char* filePath)
+	{
+		//フックされなかったので、マップチップを作成する。
+		auto mapChipRender = std::make_shared<MapChip>(objData, filePath);
+		m_mapChipPtrs.push_back(mapChipRender);
+	}
+
 	//初期化
 	void Level3DRender::Init(
 		const char* filePath,
@@ -31,46 +40,67 @@ namespace nsK2EngineLow
 
 		MatrixTklToLevel();
 
+		//ルートボーンはいらないため、iを1から始めることで除外できる。
 		for (int i = 1; i < m_bonelist.size(); i++) {
 			auto bone = m_bonelist[i].get();
 			if (bone->GetParentBoneNo() == 0) {
 
-				//行列からポジションを作成する
+				//マップチップを作成する。
+				//行列からポジションを作成する。
 				LevelObjectData levelObjData;
 				bone->CalcWorldTRS(levelObjData.position, levelObjData.rotation, levelObjData.scale);
 
-				//ZupとYupの変更
-				//座標
+				//ZupとYupの変更。
 				float fix = levelObjData.position.y;
 				levelObjData.position.y = levelObjData.position.z;
 				levelObjData.position.z = -fix;
-				//回転
+
 				fix = levelObjData.rotation.y;
 				levelObjData.rotation.y = levelObjData.rotation.z;
 				levelObjData.rotation.z = -fix;
-				//大きさ
+
 				std::swap(levelObjData.scale.y, levelObjData.scale.z);
-				//ZupとYupの変更　終わり
+				//Zup⇔Yup　終わり。
+
+				levelObjData.number = m_bonelist[i]->GetNo();
 
 				levelObjData.name = m_bonelist[i]->GetName();
-				std::wstring name = levelObjData.name;
-				//@があるかどうか、検索する
-				int pos = static_cast<int>(name.find('@'));
-				//@が存在していれば、@後の数字を取得する
-				if (pos != std::wstring::npos)
-				{
-					std::wstring number = name.substr(static_cast<size_t>(pos) + 1);
-					//wchar型をint型に変換している
-					levelObjData.number = std::stoi(number.c_str());
-				}
+
+				//パスの作成
+				wchar_t filePath[256];
+				swprintf_s(filePath, L"Assets/modelData/%s.tkm", levelObjData.name);
+				//マルチバイトに変換 wchar→ char
+				//ベースの文字列。
+				size_t origsize = wcslen(filePath) + 1;
+				//変換した文字列
+				size_t convertedChars = 0;
+				//マルチバイト格納用
+				char strCon[] = "";
+				//サイズ
+				size_t strConSize = (strlen(strCon) + 1) * 2;
+				//変換後のサイズ。
+				const size_t newsize = origsize * 2;
+				//連結後のサイズ
+				char* cFilePath = new char[newsize + strConSize];
+				//マルチバイトに変換する。入りきらないものは切りすて
+				wcstombs_s(&convertedChars, cFilePath, newsize, filePath, _TRUNCATE);
+				//文字列の連結
+				_mbscat_s((unsigned char*)cFilePath, newsize + strConSize, (unsigned char*)strCon);
 
 				//Hookが登録済みならばマップチップは作成不要
-					//false のままなら作成する。
+				//false のままなら作成する。
 				bool isHooked = false;
 				//hookFuncになにも入っていない＝Sobjectには何もない。
 				if (hookFunc != nullptr) {
 					isHooked = hookFunc(levelObjData);
 				}
+
+				//hookがfalseなままならば。
+				if (isHooked == false) {
+					//マップチップレンダーを作成。
+					CreateMapChip(levelObjData, cFilePath);
+				}
+
 			}
 		}
 	}
@@ -126,5 +156,13 @@ namespace nsK2EngineLow
 			m_bonelist.push_back(std::move(bone));
 			//コピー終わり。
 			});
+	}
+
+	void Level3DRender::Draw(RenderContext& rc)
+	{
+		for (auto mapChipRenderPtr : m_mapChipPtrs)
+		{
+			mapChipRenderPtr->Draw(rc);
+		}
 	}
 }
