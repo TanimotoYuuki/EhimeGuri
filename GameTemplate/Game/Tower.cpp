59568c5b57c88ac488d78d51255e54f30cb458f2
谷本優樹 <143520538+TanimotoYuuki@users.noreply.github.com>
2025-06-27 11:51:01 +0900

    当たり判定の不具合の修正 (#134)
    Co-authored-by: 谷本優樹 <yuki016.kawa@gmail.com>

--- GameTemplate/Game/Tower.cpp
+++ GameTemplate/Game/Tower.cpp
@@ -9,8 +9,8 @@ namespace
 	const float SPEED = 500.0f; // 移動速度。
 
 	const Vector3 SCALE(1.5f, 3.0f, 3.0f);
-	const Vector3 COLLISION_HEIGHT(0.0f, 50.0f, 0.0f);// コリジョンの高さ。
-	const Vector3 COLLISION_SIZE(365.0f, 5.0f, 225.0f);// コリジョンの大きさ。
+	const Vector3 COLLISION_HEIGHT(0.0f, 600.0f, 0.0f);// コリジョンの高さ。
+	const Vector3 COLLISION_SIZE(60.0f, 5.0f, 60.0f);// コリジョンの大きさ。
 
 }
 
@@ -20,7 +20,7 @@ bool Tower::Start()
 	m_modelRender.Init("Assets/modelData/Tower/Tower.tkm", 0, 0, enModelUpAxisZ, false, true);
 
 	m_modelRender.SetScale(SCALE);
-//	m_modelRender.Update();
+	m_modelRender.Update();
 	//当たり判定
 	m_physicsStaticObject.CreateFromModel(m_modelRender.GetModel(), m_modelRender.GetModel().GetWorldMatrix());
 
@@ -106,9 +106,52 @@ void Tower::Move()
 	}
 	m_modelRender.Update();
 	m_modelRender.SetPosition(m_position);
+
+	//プレイヤーがジャンプしているとき
+	if (!m_player->m_characterController.IsOnGround())
+	{
+		//プレイヤーがタワーでジャンプしたら処理しない。
+		if (m_playerTowerJumpFlag)
+		{
+			return;
+		}
+
+		//タワーのコリジョンの衝突判定
+		TowerCollisionDetection();
+
+		//タワーのコリジョンに衝突していたら
+		if (!m_isNotHitTowerCollision)
+		{
+			//慣性を考慮したジャンプをする。
+			m_player->m_moveSpeed.y += moveSpeed.y;
+			m_playerTowerJumpFlag = true;
+		}
+	}
+	//プレイヤーがジャンプしていないとき
+	else
+	{
+		//地面に着地したらフラグをリセット
+		m_isHitTowerCollision = false;
+		m_isNotHitTowerCollision = false;
+		m_playerTowerJumpFlag = false;
+	}
 }
 
 void Tower::Render(RenderContext& rc)
 {
 	m_modelRender.Draw(rc);
+}
+
+void Tower::TowerCollisionDetection()
+{
+	//タワーのコリジョンに衝突したら
+	if (m_collisionObject->IsHit(m_player->m_characterController))
+	{
+		m_isHitTowerCollision = true;
+	}
+	//タワー以外のコリジョンに衝突したら
+	else
+	{
+		m_isNotHitTowerCollision = true;
+	}
 }
